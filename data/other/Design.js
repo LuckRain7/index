cd1046285cffebdc4f083548d75fc032ba42a413
LuckRain7 <763839938@qq.com>
2020-07-16 16:23:40 +0800

    docs: update

---
+++ data/other/Design.js
@@ -0,0 +1,85 @@
+/*
+ * @Author       : 震雨 LuckRain7
+ * @Date         : 2020-07-16
+ * @Description  : Design 设计相关
+ * @ Love and Peace
+ */
+
+// { name: "", url: "" },
+// { name: "", url: "" },
+// { name: "", url: "" },
+
+export default {
+  name: "Design 设计相关",
+  children: [
+    {
+      name: "uiGradients - Beautiful colored gradients（渐变色）",
+      url: "https://uigradients.com/",
+    },
+    {
+      name: "玄色 - 中国传统颜色手册 | Chinese Color Cheatsheet ",
+      url: "https://colors.ichuantong.cn/",
+    },
+    {
+      name:
+        "Material Design Color Palette Generator - Material Palette（颜色）",
+      url: "https://www.materialpalette.com/",
+    },
+    {
+      name: "illlustrations - open source illustrations kit（卡通图片）",
+      url: "https://illlustrations.co/",
+    },
+    { name: "免费字体 font space", url: "https://www.fontspace.com/" },
+    { name: "配色类 Color Hunt", url: "http://colorhunt.co" },
+    {
+      name: "Adobe Color CC 配色神器",
+      url: "https://color.adobe.com/zh/create/color-wheel",
+    },
+    {
+      name: "WEBSAFECOLORS Web 安全色",
+      url: "http://www.bootcss.com/p/websafecolors",
+    },
+    {
+      name: "LOLCOLORS 色彩搭配",
+      url: "https://www.webdesignrankings.com/resources/lolcolors",
+    },
+    { name: "COLOR 随机寻找颜色灵感", url: "http://color.aurlien.net" },
+    { name: "uiGradients 渐变配色", url: "https://uigradients.com" },
+    { name: "CSS3 渐变效果工具", url: "https://www.bestvist.com/css-gradient" },
+    { name: "中国色", url: "http://zhongguose.com" },
+    { name: "日本色", url: "http://nipponcolors.com" },
+    {
+      name: "unDraw 免费可商用自定义颜色图表库",
+      url: "https://undraw.co/illustrations",
+    },
+    {
+      name:
+        "For Web Designers And Developers — Smashing Magazine （网页开发设计灵感 | 纯英文）",
+      url: "https://www.smashingmagazine.com/",
+    },
+    {
+      name: "Instagram Fonts Generator (𝓬𝓸𝓹𝔂 𝕒𝕟𝕕 𝓅𝒶𝓈𝓉𝑒) ― IGFonts.io",
+      url: "https://igfonts.io/",
+    },
+    {
+      name: "Color Picker — A handy design tool from Color Supply（色彩搭配）",
+      url: "https://colorsupplyyy.com/app",
+    },
+    {
+      name: "艺术字体在线生成器 艺术字转换器 字体转换器",
+      url: "https://www.qt86.com/",
+    },
+    {
+      name: "📙 Emojipedia — 😃 Home of Emoji Meanings 💁👌🎍😍",
+      url: "https://emojipedia.org/",
+    },
+    {
+      name: "图标下载，ICON(SVG/PNG/ICO/ICNS)图标搜索下载 - Easyicon",
+      url: "https://www.easyicon.net/",
+    },
+    {
+      name: "Feature Icons - 开源图标库 提供一些 24x24 的常用图标",
+      url: "https://github.com/feathericons/feather",
+    },
+  ],
+};
--- data/other/Images.js
+++ data/other/Images.js
@@ -7,5 +7,40 @@
 
 export default {
   name: "Images 图片工具",
-  children: [{ name: "Logoly.Pro —— [PH风格生成器]", url: "https://www.logoly.pro" }],
+  children: [
+    { name: "Logoly.Pro —— [PH风格Logo生成器]",
+     url: "https://www.logoly.pro" },
+    {
+      name: "Carbon（代码图片生成）",
+      url: "https://carbon.now.sh/",
+    },
+    {
+      name: "诺基亚短信图片生成器",
+      url: "https://zzkia.noddl.me:8020/?from=www.shadiao.app",
+    },
+    { name: "wallhaven 壁纸", url: "https://wallhaven.cc" },
+    { name: "Artvee - 收集古典艺术品的照片和海报", url: "https://artvee.com/" },
+    {
+      name: "世界著名的免费摄影图库 素材中国, 高清壁纸 - PxHere 摄影图库",
+      url: "https://pxhere.com/",
+    },
+    {
+      name: "Beautiful Free Images & Pictures | Unsplash",
+      url: "https://unsplash.com",
+    },
+    { name: "Free stock photos - Kaboompics", url: "https://kaboompics.com/" },
+    {
+      name: "泼辣有图 | Polayoutu - Free Chinese stock photos",
+      url: "https://www.polayoutu.com/",
+    },
+    {
+      name: "Wallpaper Abyss - HD Wallpapers, Background Images",
+      url: "https://wall.alphacoders.com/",
+    },
+    { name: "Simple Desktops - 极简桌面壁纸", url: "http://simpledesktops.com/" },
+  ],
 };
+
+// { name: "", url: "" },
+// { name: "", url: "" },
+// { name: "", url: "" },
